20016f222cebb45a9e1275c829fe1c99a5d70fa2
James Kroeger <69898535+qlyde@users.noreply.github.com>
2022-08-22 04:58:10 +1000

    Time: 28 ms (33.70%), Space: 5.7 MB (91.05%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) return false;
+
+        auto n = x;
+        auto rev = 0ULL;
+
+        while (n > 0) {
+            rev = rev * 10 + n % 10;
+            n /= 10;
+        }
+
+        return x == rev;
+    }
+};
