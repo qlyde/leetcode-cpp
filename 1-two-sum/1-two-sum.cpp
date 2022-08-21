0fdf2fba007645242750c71000f61d7a8a1e6425
James Kroeger <69898535+qlyde@users.noreply.github.com>
2022-08-22 04:49:24 +1000

    Time: 23 ms (57.95%), Space: 10.8 MB (47.75%) - LeetHub

---
+++ 1-two-sum/1-two-sum.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        auto m = unordered_map<int, int>{};
+
+        for (auto i = 0; i < nums.size(); ++i) {
+            auto const n = target - nums[i];
+            if (m.find(n) != m.end()) return {m[n], i};
+            m[nums[i]] = i;
+        }
+
+        return {};
+    }
+};
