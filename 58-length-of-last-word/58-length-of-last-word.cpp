b988b9375284b09464296dba49103260f8c18ea6
James Kroeger <69898535+qlyde@users.noreply.github.com>
2022-08-22 05:26:58 +1000

    Time: 3 ms (58.90%), Space: 7 MB (6.76%) - LeetHub

---
+++ 58-length-of-last-word/58-length-of-last-word.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        auto iss = istringstream{s};
+        auto tkns = vector<string>{istream_iterator<string>{iss}, {}};
+        return tkns.back().size();
+    }
+};
