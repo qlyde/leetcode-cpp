5d6ed21de3c9fbf99096b86bd3cf17309ddda5db
James Kroeger <69898535+qlyde@users.noreply.github.com>
2022-08-22 06:46:33 +1000

    Time: 0 ms (100.00%), Space: 6.4 MB (80.24%) - LeetHub

--- 119-pascals-triangle-ii/119-pascals-triangle-ii.cpp
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.cpp
@@ -2,9 +2,8 @@ class Solution {
 public:
     vector<int> getRow(int rowIndex) {
         vector<int> ret{1};
-        for (unsigned i = 1; i <= rowIndex; ++i) {
+        for (unsigned i = 1; i <= rowIndex; ++i)
             ret.emplace_back((long long) ret.back() * (rowIndex - i + 1) / i);
-        }
         return ret;
     }
 };
