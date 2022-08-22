66105e4aa4ec2d056e7584b577a7fde7f2103725
James Kroeger <69898535+qlyde@users.noreply.github.com>
2022-08-22 10:08:39 +1000

    Time: 16 ms (96.13%), Space: 16.8 MB (86.59%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        // sum = 2(x + y + z + ...) + ret
         return accumulate(nums.begin(), nums.end(), 0, bit_xor<int>());
     }
 };
