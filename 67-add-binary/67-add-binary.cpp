6387d48f2c57a99eedbfc07858bc5d973b6747c4
James Kroeger <69898535+qlyde@users.noreply.github.com>
2022-08-22 10:52:56 +1000

    Time: 4 ms (59.22%), Space: 6.3 MB (57.66%) - LeetHub

--- 67-add-binary/67-add-binary.cpp
+++ 67-add-binary/67-add-binary.cpp
@@ -8,20 +8,20 @@ public:
         for (p = a.size() - 1, q = b.size() - 1; p >= 0 && q >= 0; --p, --q) {
             if (a[p] == '1' && b[q] == '1') carry += 2;
             else if (a[p] == '1' || b[q] == '1') ++carry;
-            
+
             if (carry % 2 == 1) ret.insert(0, 1, '1');
             else ret.insert(0, 1, '0');
             carry >>= 1;
         }
-        
+
         for (; p >= 0; --p) {
             if (a[p] == '1') ++carry;
-            
+
             if (carry % 2 == 1) ret.insert(0, 1, '1');
             else ret.insert(0, 1, '0');
             carry >>= 1;
         }
-        
+
         if (carry) ret.insert(0, 1, '1');
         return ret;
     }
