fd81457c1c124c319dd87191d5aa0be32cfeaad0
James Kroeger <69898535+qlyde@users.noreply.github.com>
2022-08-22 10:56:13 +1000

    Time: 3 ms (76.38%), Space: 6.3 MB (57.66%) - LeetHub

--- 67-add-binary/67-add-binary.cpp
+++ 67-add-binary/67-add-binary.cpp
@@ -1,23 +1,16 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-        int carry{0}, p = a.size() - 1, q = b.size() - 1;
+        int p = a.size() - 1, q = b.size() - 1;
         if (p < q) return addBinary(b, a);
 
         string ret{};
-        for (p = a.size() - 1, q = b.size() - 1; p >= 0 && q >= 0; --p, --q) {
-            if (a[p] == '1' && b[q] == '1') carry += 2;
-            else if (a[p] == '1' || b[q] == '1') ++carry;
-
-            if (carry % 2 == 1) ret.insert(0, 1, '1');
-            else ret.insert(0, 1, '0');
-            carry >>= 1;
-        }
-
-        for (; p >= 0; --p) {
+        int carry{0};
+        for (; p >= 0 || q >= 0; --p, --q) {
             if (a[p] == '1') ++carry;
+            if (q > -1 && b[q] == '1') ++carry;
 
-            if (carry % 2 == 1) ret.insert(0, 1, '1');
+            if (carry & 1) ret.insert(0, 1, '1');
             else ret.insert(0, 1, '0');
             carry >>= 1;
         }
