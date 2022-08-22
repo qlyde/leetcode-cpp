2fd8b090be57c015ba3ba98cc751a51fd025c785
James Kroeger <69898535+qlyde@users.noreply.github.com>
2022-08-23 06:00:12 +1000

    Time: 28 ms (63.09%), Space: 19.6 MB (59.64%) - LeetHub

---
+++ 169-majority-element/169-majority-element.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unsigned sz = nums.size();
+        unsigned target = sz / 2;
+        unordered_map<int, unsigned> cnt;
+        for (auto const& num : nums) {
+            if (cnt.find(num) == cnt.end()) cnt[num] = 1;
+            else ++cnt[num];
+            if (cnt[num] > target) return num;
+        }
+        return -1; // never
+    }
+};
