a6a1320408692ed2cd42ee0ced4bf354b3fd2411
James Kroeger <69898535+qlyde@users.noreply.github.com>
2022-08-22 11:37:13 +1000

    Time: 40 ms (11.99%), Space: 24.1 MB (11.89%) - LeetHub

--- 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.cpp
+++ 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.cpp
@@ -15,11 +15,11 @@ public:
         // -10 -3 -1 0 5 6 9
         // -10 -3 -1   5 6 9
         if (nums.empty()) return nullptr;
-        
+
         auto m = nums.begin(); advance(m, nums.size() / 2);
         auto l = vector<int>(nums.begin(), m);
         auto r = vector<int>(next(m), nums.end());
-        
+
         TreeNode* root = new TreeNode(*m);
         root->left = sortedArrayToBST(l);
         root->right = sortedArrayToBST(r);
