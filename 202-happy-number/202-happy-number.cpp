a0c5d0e8c6a4af9bb350eab4af055a64ab22d86e
James Kroeger <69898535+qlyde@users.noreply.github.com>
2022-08-23 21:03:15 +1000

    Time: 7 ms (21.22%), Space: 6.3 MB (27.17%) - LeetHub

---
+++ 202-happy-number/202-happy-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        int sqsum;
+        unordered_set<int> seen;
+        while (sqsum != 1) {
+            sqsum = 0;
+            while (n > 0) {
+                int m = n % 10;
+                sqsum += m * m;
+                n /= 10;
+            }
+            
+            if (seen.find(sqsum) != seen.end()) return false;
+            seen.insert(sqsum);
+            n = sqsum;
+        }
+
+        return true;
+    }
+};
