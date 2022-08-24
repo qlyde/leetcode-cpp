d3047dec72f8f2cc1456899588bedbce5e60dbe3
James Kroeger <69898535+qlyde@users.noreply.github.com>
2022-08-24 21:00:57 +1000

    Time: 130 ms (67.56%), Space: 55.2 MB (6.38%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        return set<int>(nums.begin(), nums.end()).size() != nums.size();
+        return unordered_set<int>(nums.begin(), nums.end()).size() != nums.size();
     }
 };
