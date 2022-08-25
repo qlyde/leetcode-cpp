23ebbe44c773c22cc71add7ab2ef66d8a4bc222e
James Kroeger <69898535+qlyde@users.noreply.github.com>
2022-08-25 22:18:33 +1000

    Time: 13 ms (60.37%), Space: 7.4 MB (46.64%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        auto mp = unordered_map<char, int>{};
+        for (auto const& ch : s) ++mp[ch];
+        for (auto const& ch : t) if (!mp[ch]--) return false;
+        for (auto const& [ch, cnt] : mp) if (cnt) return false;
+        return true;
+    }
+};
