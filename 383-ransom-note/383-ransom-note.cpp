f3ddb9d9ebc76e92fcd33fd4f2de01c6a18686b1
James Kroeger <69898535+qlyde@users.noreply.github.com>
2022-08-25 19:48:24 +1000

    Time: 43 ms (22.29%), Space: 9 MB (15.41%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
+        if (ransomNote.length() > magazine.length()) return false;
+
         unordered_map<char, int> letters;
         for (char const& letter : magazine) {
             if (letters.find(letter) != letters.end()) ++letters[letter];
