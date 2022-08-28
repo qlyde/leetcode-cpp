dfdab9d7f8834ad2cc3bca6b69f4c2104104d1a6
James Kroeger <69898535+qlyde@users.noreply.github.com>
2022-08-28 19:59:01 +1000

    Time: 32 ms (69.11%), Space: 19.2 MB (83.58%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int i = 0;
-        for (int j = 0; j < nums.size(); ++j)
-            if (nums[j]) nums[i++] = nums[j];
-        for (; i < nums.size(); ++i) nums[i] = 0;
+        for (int i = 0, j = 0; j < nums.size(); ++j)
+            if (nums[j]) swap(nums[i++], nums[j]);
     }
 };
