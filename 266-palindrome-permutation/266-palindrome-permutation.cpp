c3bd882659db15061572619a6204ded606da031b
James Kroeger <69898535+qlyde@users.noreply.github.com>
2022-08-28 17:35:48 +1000

    Time: 0 ms (100.00%), Space: 6.2 MB (34.65%) - LeetHub

--- 266-palindrome-permutation/266-palindrome-permutation.cpp
+++ 266-palindrome-permutation/266-palindrome-permutation.cpp
@@ -3,19 +3,9 @@ public:
     bool canPermutePalindrome(string s) {
         auto cnts = unordered_map<char, int>{};
         for (auto const& ch : s) ++cnts[ch];
-
-        auto const even = s.length() & 1 == 0;
-        auto odd_ch = false;
-        for (auto const& [ch, cnt] : cnts) {
-            if (cnt & 1 != 0) {
-                if (odd_ch && !even) return false;
-                odd_ch = true;
-            } else if (cnt & 1 == 0 && even) return false;
-        }
         
-        return true;
+        return accumulate(cnts.begin(), cnts.end(), 0, [](auto const& p, auto const& e) {
+            return p + e.second % 2;
+        }) <= 1;
     }
 };
-
-// even length -> no odd counts allowed
-// odd length -> one odd count allowed
