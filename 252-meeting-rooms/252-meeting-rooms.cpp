3f1150c6d0d886db7309a2caa9103e9c62399780
James Kroeger <69898535+qlyde@users.noreply.github.com>
2022-08-28 17:14:36 +1000

    Time: 32 ms (37.39%), Space: 11.7 MB (77.19%) - LeetHub

---
+++ 252-meeting-rooms/252-meeting-rooms.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool canAttendMeetings(vector<vector<int>>& intervals) {
+        if (intervals.empty()) return true;
+        sort(intervals.begin(), intervals.end());
+        auto const sz = intervals.size();
+        for (auto i = 0; i < sz - 1; ++i)
+            if (intervals[i][1] > intervals[i + 1][0]) return false;
+        return true;
+    }
+};
