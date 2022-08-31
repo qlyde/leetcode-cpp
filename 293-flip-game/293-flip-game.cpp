097ced3ce5d20f9660c06ae2b99ec1995899a5f3
James Kroeger <69898535+qlyde@users.noreply.github.com>
2022-09-01 07:22:38 +1000

    Time: 0 ms (100.00%), Space: 7.4 MB (49.58%) - LeetHub

--- 293-flip-game/293-flip-game.cpp
+++ 293-flip-game/293-flip-game.cpp
@@ -4,10 +4,9 @@ public:
         vector<string> ret;
         for (int i = 0; i < currentState.size() - 1; ++i) {
             if (currentState[i] == '+' && currentState[i + 1] == '+') {
-                string copy = currentState;
-                copy[i] = '-';
-                copy[i + 1] = '-';
-                ret.push_back(copy);
+                currentState[i] = currentState[i + 1] = '-';
+                ret.push_back(currentState);
+                currentState[i] = currentState[i + 1] = '+';
             }
         }
         return ret;
