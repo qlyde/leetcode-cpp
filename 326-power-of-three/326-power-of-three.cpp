9a0ba696317d50db02cd504912b5655fe4e85279
James Kroeger <69898535+qlyde@users.noreply.github.com>
2022-09-06 06:28:45 +1000

    Time: 60 ms (6.06%), Space: 6.2 MB (8.49%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if (n < 1) return false;
-        double x = log10(n) / log10(3);
-        return x == (int) x;
+        return fmod((log10(n) / log10(3)), 1) == 0;
     }
 };
 
