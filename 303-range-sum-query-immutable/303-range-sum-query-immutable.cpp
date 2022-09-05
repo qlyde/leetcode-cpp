8849e9ba26649c546cffc1917e74b4ca05d0c0f5
James Kroeger <69898535+qlyde@users.noreply.github.com>
2022-09-06 06:16:26 +1000

    Time: 33 ms (72.12%), Space: 17.2 MB (8.26%) - LeetHub

--- 303-range-sum-query-immutable/303-range-sum-query-immutable.cpp
+++ 303-range-sum-query-immutable/303-range-sum-query-immutable.cpp
@@ -1,12 +1,14 @@
 class NumArray {
 public:
-    NumArray(vector<int>& nums) : sums_{} {
-        partial_sum(nums.begin(), nums.end(), back_inserter(sums_));
+    NumArray(vector<int>& nums) : sums_{nums[0]} {
+        // partial_sum(nums.begin(), nums.end(), back_inserter(sums_));
+        int size = nums.size();
+        for (int i = 1; i < size; ++i)
+            sums_.push_back(sums_[i - 1] + nums[i]);
     }
 
     int sumRange(int left, int right) {
-        if (left) return sums_[right] - sums_[left - 1];
-        return sums_[right];
+        return sums_[right] - (left ? sums_[left - 1] : 0);
     }
 
 private:
@@ -18,3 +20,4 @@ private:
  * NumArray* obj = new NumArray(nums);
  * int param_1 = obj->sumRange(left,right);
  */
+
