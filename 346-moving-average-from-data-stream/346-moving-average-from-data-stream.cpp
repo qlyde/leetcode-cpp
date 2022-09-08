02a9ffb56703a9f7f8e795c68d681427c1feb682
James Kroeger <69898535+qlyde@users.noreply.github.com>
2022-09-09 09:47:34 +1000

    Time: 27 ms (79.05%), Space: 14.1 MB (15.79%) - LeetHub

---
+++ 346-moving-average-from-data-stream/346-moving-average-from-data-stream.cpp
@@ -0,0 +1,31 @@
+class MovingAverage {
+public:
+    MovingAverage(int size) 
+    : size_{size}
+    , window_{}
+    , n_{0}
+    , sum_{0} {}
+
+    double next(int val) {
+        ++n_;
+        window_.push_back(val);
+        if (n_ > size_) {
+            sum_ -= window_.front();
+            window_.pop_front();
+        }
+        sum_ += val;
+        return (double) sum_ / min(n_, size_);
+    }
+
+private:
+    int size_;
+    deque<int> window_;
+    int n_;
+    int sum_;
+};
+
+/**
+ * Your MovingAverage object will be instantiated and called as such:
+ * MovingAverage* obj = new MovingAverage(size);
+ * double param_1 = obj->next(val);
+ */
