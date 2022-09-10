6fd11b99e4c81730015306a0110f77dac82fb105
James Kroeger <69898535+qlyde@users.noreply.github.com>
2022-09-10 16:17:58 +1000

    Time: 179 ms (5.58%), Space: 32.3 MB (86.72%) - LeetHub

---
+++ 359-logger-rate-limiter/359-logger-rate-limiter.cpp
@@ -0,0 +1,20 @@
+class Logger {
+public:
+    Logger()
+    : messages_{} {}
+    
+    bool shouldPrintMessage(int timestamp, string message) {
+        if (messages_.find(message) != messages_.end() && timestamp - messages_[message] < 10) return false;
+        messages_[message] = timestamp;
+        return true;
+    }
+
+private:
+    unordered_map<string, int> messages_;
+};
+
+/**
+ * Your Logger object will be instantiated and called as such:
+ * Logger* obj = new Logger();
+ * bool param_1 = obj->shouldPrintMessage(timestamp,message);
+ */
